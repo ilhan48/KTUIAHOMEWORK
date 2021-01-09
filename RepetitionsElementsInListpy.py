4a9ba39daccf2c727a5e143f9c23996ca8c7d76e
İlhan Ödün <58470523+ilhan48@users.noreply.github.com>
2021-01-09 04:43:33 +0300

    Add files via upload

---
+++ RepetitionsElementsInListpy.py
@@ -0,0 +1,57 @@
+print(""" 
+************************************************************************
+
+Application that returns the number of repetitions of elements in the list.
+*Enter as many values ​​as you want to prepare a dictionary in the program.
+*The moment you press the 'q', it will give you feedback as it has reached its purpose.
+                                                                   *** by İLHAN ÖDÜN***
+************************************************************************
+""")
+yourlist = []
+key = []
+values = []
+value = 0
+while True:
+    default = input("Please enter a value: ")
+    if default == 'q':
+        break
+    else:
+        yourlist.append(default)
+    
+print("The list your entered: {}".format(yourlist))
+for i in yourlist:
+    for j in range (0,len(yourlist)):
+        if yourlist[j] == i:
+            value += 1
+        setvalue = value
+        keynumber = i 
+    key.append(keynumber)
+    values.append(setvalue)
+        
+        
+    value = 0
+    
+
+dictionary = dict(zip(key,values))
+print(dictionary)
+
+
+   
+           
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+
+    
+
+    
+        
+    
+        
